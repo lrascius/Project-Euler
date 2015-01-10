93621110f1a716f50ca6c2e6bb3db7aed286f3f1
Lukas Rascius <lrascius93@gmail.com>
2015-01-09 21:16:54 -0500

    Adding problem12.py

---
+++ problem12.py
@@ -0,0 +1,16 @@
+#Project Euler
+#Problem 12: Power digit sum
+#Description: 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#			  What is the sum of the digits of the number 2^1000?
+
+import math
+
+num = pow(2,1000)
+
+result = 0
+
+while num >= 1:
+	result += num % 10
+	num = num / 10
+
+print result
