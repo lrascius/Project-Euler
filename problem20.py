934d56bca805e2882d0498e268282c9e9505e968
Lukas Rascius <lrascius93@gmail.com>
2015-01-28 15:33:47 -0500

    Adding problem 20

---
+++ problem20.py
@@ -0,0 +1,18 @@
+#Project Euler
+#Problem 15: Lattice Paths
+#Description: n! means n x (n - 1) x ... x 3 x 2 x 1
+# 		      For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+# 			  and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+# 			  Find the sum of the digits in the number 100!
+
+import math
+
+# for x in range(1,10):
+# 	print 
+largeNum = math.factorial(100);
+sumTot = 0
+
+for i in range(0,len(str(largeNum))):
+	sumTot+=largeNum % 10
+	largeNum/=10
+print sumTot
